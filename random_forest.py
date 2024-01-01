8c3de01bedabac1dd7d926ca48ba9db36f1e50b2
Tanmay Patil <tany.patil77@gmail.com>
2024-01-01 18:11:15 +0530

    random forest

--- random_forest.py
+++ random_forest.py
@@ -1,27 +1,28 @@
 # Import RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import accuracy_score, classification_report
 
-def randomized_search_class(params, runs=16, reg=RandomForestClassifier(random_state=2, n_jobs=-1)):
+def randomized_search_class(X_train,y_train,X_test,y_test,params,scoring, reg,runs=16):
 
-    # Instantiate RandomizedSearchCV as grid_reg
-    rand_reg = RandomizedSearchCV(reg, params, n_iter=runs, scoring='neg_mean_squared_error', 
+    # Instantiate RandomizedSearchCV as grid_classifier model
+    rand_classify = RandomizedSearchCV(reg, params,n_iter=runs, scoring=scoring, 
                                   cv=10, n_jobs=-1, random_state=2)
     
-    # Fit grid_reg on X_train and y_train
-    rand_reg.fit(X_train, y_train)
+    # Fit grid_classifier on X_train and y_train
+    rand_classify.fit(X_train, y_train)
 
     # Extract best estimator
-    best_model = rand_reg.best_estimator_
+    best_model = rand_classify.best_estimator_
 
     # Extract best params
-    best_params = rand_reg.best_params_
+    best_params = rand_classify.best_params_
 
     # Print best params
     print("Best params:", best_params)
     
     # Compute best score
-    best_score = np.sqrt(-rand_reg.best_score_)
+    best_score = rand_classify.best_score_
 
     # Print best score
     print("Training score: {:.3f}".format(best_score))
@@ -29,11 +30,7 @@ def randomized_search_class(params, runs=16, reg=RandomForestClassifier(random_s
     # Predict test set labels
     y_pred = best_model.predict(X_test)
     
-    # Import mean_squared_error from sklearn.metrics as MSE 
-    from sklearn.metrics import mean_squared_error as MSE
-
-    # Compute rmse_test
-    rmse_test = MSE(y_test, y_pred)**0.5
-
-    # Print rmse_test
-    print('Test set score: {:.3f}'.format(rmse_test))
+    # calculate accuracy , precision , recall on the best model
+    report = classification_report(y_test, y_pred)
+    print(f"report post randomised grid search on best model - Validation data :\n {report}")
+    return (best_model,y_pred)
